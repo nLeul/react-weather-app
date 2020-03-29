82c84550624463073dd0d46b54fd28cd2a9b84fb
nLeul <leulnecha@gmail.com>
2020-03-29 03:08:24 -0500

    fetch from API succesfull

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import './App.css';
 import WeatherCard from './components/weather';
 
 
-function App() {
+const App = () => {
+  // const city = 'london';
+
+  // const country = 'uk';
+
+  // const APPID = 'b93aec3cb64ff501cac71c58001a9518';//API Key
+  // api.openweathermap.org/data/2.5/weather?q={city name},{state}&appid={your api key}
+  // api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b93aec3cb64ff501cac71c58001a9518
+  // fetch(`api.openweathermap.org/data/2.5/weather?q=${city},${country}& ${APPID}`)
+  const data = async () => {
+    const apiRes = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b93aec3cb64ff501cac71c58001a9518`);
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+  console.log(data());
   return (
     <div className="App">
-      <WeatherCard />
+      <WeatherCard temp="35" condition="Rain" city="Newyork" country="US" />
+      <WeatherCard temp="40" condition="Rain" city="paris" country="Fr" />
     </div>
   );
 }
