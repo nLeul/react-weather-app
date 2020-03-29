24144da63a4252cfb59cdf47fc08c70a30e6aad8
nLeul <leulnecha@gmail.com>
2020-03-28 20:52:11 -0500

    add weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WeatherCard from './components/weather';
+
 
 function App() {
   return (
     <div className="App">
-
+      <WeatherCard />
     </div>
   );
 }
---
+++ src/components/weather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WeatherCard = () => {
+    return (
+        <div className="card">
+            <h1>New York</h1>
+            <h3>US</h3>
+            <img src="" alt="weather icon" />
+            <h1>30 °C </h1>
+            <h3>Cloudy</h3>
+        </div>
+    );
+}
+
+export default WeatherCard;
