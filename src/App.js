afe4ac057215c0f0ec14a334752d5307e9e9cf0d
nLeul <leulnecha@gmail.com>
2020-03-29 04:03:59 -0500

    add useState Hook for input and search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import WeatherCard from './components/weather';
 
 
 const App = () => {
-  // const city = 'london';
+
+  const [city, setCity] = useState('london,gb');
+  // setCity()
+  //  const city = 'london';
 
   // const country = 'uk';
 
@@ -13,15 +16,27 @@ const App = () => {
   // api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b93aec3cb64ff501cac71c58001a9518
   // fetch(`api.openweathermap.org/data/2.5/weather?q=${city},${country}& ${APPID}`)
   const data = async () => {
-    const apiRes = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b93aec3cb64ff501cac71c58001a9518`);
+    const apiRes = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=b93aec3cb64ff501cac71c58001a9518&units=imperial`);
     const resJSON = await apiRes.json();
     return resJSON;
   };
-  console.log(data());
+  // data().then(res =>{
+  //   console.log(res.name);
+  //   console.log(res.sys.country);
+  //   console.log(res.main.feels_like);
+  //   console.log(res.weather[0].main);
+
+  // });
+
+  
   return (
     <div className="App">
       <WeatherCard temp="35" condition="Rain" city="Newyork" country="US" />
-      <WeatherCard temp="40" condition="Rain" city="paris" country="Fr" />
+      <WeatherCard temp="40" condition="Rain" city="Paris" country="Fr" />
+      <form action="">
+        <input value={city} onChange={(event) => { setCity(event.target.value) }}></input>
+        <button onClick={}>Search</button>
+      </form>
     </div>
   );
 }
